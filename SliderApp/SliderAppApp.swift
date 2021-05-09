60f68fad2ca3748eb873a33393b15f383e4664d4
moe301 <sugar17xyz@gmail.com>
2021-05-09 17:06:43 +0900

    SliderAppを作成

---
+++ SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by fullerene-01 on 2021/04/15.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
