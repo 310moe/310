c49e4d9fc480759724ce03ed4700a8f562e342d6
moe301 <sugar17xyz@gmail.com>
2021-05-09 17:09:30 +0900

    MapAppを作成

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by fullerene-01 on 2021/04/13.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
