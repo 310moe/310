dec498a07e1696b04d0f0378bd06d612b74417c7
moe301 <sugar17xyz@gmail.com>
2021-05-09 17:08:26 +0900

    CardAppを作成

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by fullerene-01 on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
