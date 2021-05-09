254b8fede055b7c06bfbf11fea5d2d235f510b78
moe301 <sugar17xyz@gmail.com>
2021-05-09 17:01:37 +0900

    ListAppを作成

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by fullerene-01 on 2021/04/05.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
